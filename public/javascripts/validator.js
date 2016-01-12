991ef749e3567d267529299aba3c7fc44f3a9b76
Corey Yaspan <cspan00@gmail.com>
2016-01-11 22:06:00 -0700

    redeploy to heroku

--- public/javascripts/validator.js
+++ public/javascripts/validator.js
@@ -1,17 +1,4 @@
 
 module.exports = {
 
-  validate: function(url){
-    var extension = url.substr(url.length - 3)
-
-    if(extension === 'jpg' || extension === 'png' || extension === 'gif'){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-
-
-}
+  v
