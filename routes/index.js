df6b6ee848fc7fa6ef57376e3fb8000969384612
Corey Yaspan <cspan00@gmail.com>
2016-01-11 19:35:10 -0700

    linked index page to local database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')({
+  client: 'pg',
+  connection: 'postgres://localhost/mosaic'
+})
+
+function getImages(){
+  return knex('pictures')
+}
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getImages().select().then(function(result){
+      console.log('**************');
+      console.log(result);
+
+      res.render('index', {pictures: result})
+  })
+
+
+
 });
 
 module.exports = router;
